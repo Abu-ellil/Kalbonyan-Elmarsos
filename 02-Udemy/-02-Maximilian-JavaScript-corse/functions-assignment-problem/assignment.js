98d18cd6dd3aa3f5f9e1ba8147611b934b8f18ea
Abo Ellil <mr.abuellil@gmail.com>
2023-07-09 03:14:47 +0300

    del empty dir

--- 02-Udemy/-02-Maximilian-JavaScript-corse/functions-assignment-problem/assignment.js
+++
@@ -1,9 +0,0 @@
-// function sayHello(name) {
-//   console.log('Hi ' + name);
-// }
-
-
-const sayHello = (name)=> console.log('Hii '+ name);
-
-
-sayHello();
--- 02-Udemy/-02-Maximilian-JavaScript-corse/http-01-starting-setup/assets/scripts/appp.js
+++
@@ -1,6 +0,0 @@
-const fullName = "Sabe Ali Ahmed";
-
-
-const shortName = fullName.split(" ").map(n=>n[0]).join('').toLowerCase();
-
-console.log(shortName);

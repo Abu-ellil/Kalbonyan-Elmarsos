af633f8e2106e285e34064621b059f2232e381ce
Abo Ellil <mr.abuellil@gmail.com>
2023-07-09 04:31:07 +0300

    Update server.js

--- 02-Udemy/-06-MERN-Jobify/Projects/Jobify/server.js
+++ 02-Udemy/-06-MERN-Jobify/Projects/Jobify/server.js
@@ -1,40 +1,41 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 const app = express();
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import 'express-async-errors';
-import morgan from 'morgan';
+import "express-async-errors";
+import morgan from "morgan";
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
-import helmet from 'helmet';
-import xss from 'xss-clean';
-import mongoSanitize from 'express-mongo-sanitize';
-import cookieParser from 'cookie-parser';
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+
 // hello
 // db and authenticateUser
-import connectDB from './db/connect.js';
+import connectDB from "./db/connect.js";
 
 // routers
-import authRouter from './routes/authRoutes.js';
-import jobsRouter from './routes/jobsRoutes.js';
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
 
 // middleware
-import notFoundMiddleware from './middleware/not-found.js';
-import errorHandlerMiddleware from './middleware/error-handler.js';
-import authenticateUser from './middleware/auth.js';
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import authenticateUser from "./middleware/auth.js";
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // only when ready to deploy
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use(express.json());
 app.use(helmet());
@@ -42,12 +43,12 @@ app.use(xss());
 app.use(mongoSanitize());
 app.use(cookieParser());
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 // only when ready to deploy
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.use(notFoundMiddleware);
@@ -57,7 +58,10 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await mongoose.connect('mongodb+srv://mrabuellil:jopify@jopify.wyzyqqg.mongodb.net/');
+    mongoose.set("strictQuery", false); // Add this line to address the deprecation warning
+    await mongoose.connect(
+      "mongodb+srv://mrabuellil:jopify@jopify.wyzyqqg.mongodb.net/"
+    );
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`);
     });
